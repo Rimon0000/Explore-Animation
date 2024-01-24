92791c75914dcb1a26ad9721a7205aa4134554e6
Your Name <rimonron.ad@gmail.com>
2024-01-24 20:18:50 +0600

    Transform Example with a button element

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ function App() {
 
   return (
     <div className='main'>
-      <div className='box-1'></div>
+      {/* <div className='box-1'></div>
       <div className='container-box-2'>
           <div className='box-2'></div>
-      </div>
+      </div> */}
       {/* <h1>Something Cool</h1> */}
+
+      <button className='btn'>Hover me</button>
     </div>
   )
 }
