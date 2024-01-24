4269c16d70e9445fe53ce35d821febf4d7023f9a
Your Name <rimonron.ad@gmail.com>
2024-01-24 13:35:27 +0600

    transform-origin, rotate() skew() scale()

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      <h1>Animation</h1>
+    <div className='main'>
+      <div className='box-1'></div>
+      <div className='container-box-2'>
+          <div className='box-2'></div>
+      </div>
+      {/* <h1>Something Cool</h1> */}
     </div>
   )
 }
